fbaab1c585c40c86248afcf757a71b4933067237
김영광 <kim-young-gwang@gim-yeong-gwang-ui-MacBookPro.local>
2020-12-03 11:44:54 +0900

    라우터설정

---
+++ src/Pages/kyungoh/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+export default Main;
---
+++ src/Routes.js
@@ -0,0 +1,43 @@
+import React     from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import LoginGwang from './Pages/younggwang/Login/Login';
+import MainGwang from './Pages/younggwang/Main/Main';
+
+import LoginYong from './Pages/yongseok/Login/Login';
+import MainYong from './pages/yongseok/Main/Main';
+
+import LoginJae from './Pages/yeongjae/Login/Login';
+import MainJae from './Pages/yeongjae/Main/Main';
+
+import LoginOh from './Pages/kyungoh/Login/Login';
+import MainOh from './Pages/kyungoh/Main/Main';
+
+
+
+
+
+class Routes extends React.Component {
+  render() {
+    return (
+    <Router>
+        <Switch>
+            <Route exact path="/loginGwang" component={LoginGwang} />
+            <Route exact path="/mainGwang" component={MainGwang} />
+            <Route exact path="/loginYong" component={LoginYong} />
+            <Route exact path="/mainYong" component={MainYong} />
+            <Route exact path="/loginJae" component={LoginJae} />
+            <Route exact path="/mainJae" component={MainJae} />
+            <Route exact path="/loginOh" component={LoginOh} />
+            <Route exact path="/mainOh" component={MainOh} />
+        </Switch>
+    </Router>
+    )
+  }
+}
+
+export default Routes;
