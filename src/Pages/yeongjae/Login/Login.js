aab6bbd6f1b223bc057abe17b57084572781b67e
김영재 <gim-yeongjae@gim-yeongjaeui-MacBookPro.local>
2020-12-08 15:47:39 +0900

    로그인페이지 기능구현 및 메인페이지 레이아웃 완료

--- src/Pages/yeongjae/Login/Login.js
+++ src/Pages/yeongjae/Login/Login.js
@@ -1,13 +1,104 @@
+/* eslint-disable */
 import React, { Component } from 'react';
+import {Link} from "react-router-dom"
+import './Login.scss';
 
-class Login extends Component {
-    render() {
-        return (
-            <div>
-                hello
-            </div>
-        );
-    }
+class Login extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			id:"",
+			password:"",
+			hiddenPW:true,
+			producList:[],
+		};
+	}
+	//id---로그인 기능
+	handleIdChange = (e) => {
+		this.setState({id:e.target.value});
+	};
+	//pw---로그인 기능
+	handlePasswordChange=(e) => {
+		this.setState({password: e.target.value});
+	};
+	
+	checkValidation = (e) => {
+		e.preventDefault();
+		const { id, password} = this.state;
+		if(id.includes ("@") && password.length >= 4){
+			window.location.href='http://localhost:3000/main'
+		}
+		if(!id.includes("@")){
+			alert("id는 @ 필요합니다");
+		}
+		if(!password.length >= 4){
+			alert("비밀번호는 4자리 이상");
+		}
+	}
+
+	//show password --> 비밀번호 보여주기
+	// showPassword = () =>{
+	// 	//toggle식 true <->false
+	// 	this.setState({hiddenPW: !this.state.hiddenPW});
+	// }
+	
+	//15~22줄 심화 버전
+	// handleInputValueChange = (e) => {
+	// 	//구조분해 할당 = 비구조화
+	// 	const [id, value] = e.target;
+	// 	this.setState({[id] : value})
+	// };
+
+
+  render(){
+
+		// console.log("아이디", this.state.id, "pwd", this.state.password);
+		const activateBtn = 
+			(this.state.id.length && this.state.password.length) !== 0;
+		return(
+				<div className="super_log_main">
+					<div className="header_log">westagram</div>
+					<div className="main_log_page">
+						<div className="log_te_id">
+							<input 
+								id="id" 
+								type="text" 
+								placeholder="&nbsp;&nbsp;&nbsp;전화번호,사용자 이름 또는 이메일" 
+								// value={id} 
+								onChange={this.handleIdChange}
+							/>				  
+						</div>
+						<div className="log_te_pw">
+							<input 
+								id="password" 
+								type={this.state.hiddenPW ? "password" : "text"}
+								placeholder="&nbsp;&nbsp;&nbsp;비밀번호" 
+								// value={password}
+								onChange={this.handlePasswordChange}
+							/>  
+							{/* show // hide */}
+							{/* <span className="showPw" onClick={this.showPassword}>Show
+								{this.state.hiddenPW ? "Show" ! "Hide"}
+							</span> */}
+						</div>
+					
+						<div className="log_page_main_btn">
+							<Link to="/main">	
+								<button 
+									className="main_page_btn"  
+									type="button" 
+									id="btn"
+									style={{
+										backgroundColor: activateBtn ? "#045cb5" : "#9ebbe6"
+									}}
+									onClick={this.checkValidation}
+									>로그인</button>
+							</Link>
+						</div>
+						</div>
+						<div className="log_page_footer">비밀번호를 잊으셨나요?</div>
+			</div>
+	)
 }
-
+}
 export default Login;
--- src/Pages/yeongjae/Main/Main.js
+++ src/Pages/yeongjae/Main/Main.js
@@ -1,13 +1,82 @@
 import React, { Component } from 'react';
+import './Main.scss';
 
 class Main extends Component {
-    render() {
-        return (
-            <div>
-                hello
+  render() {
+    return (
+    <div className="container">
+      
+      <div className="nav">
+        <div className="nav_logo">westagram</div>
+        <div className="nav_searchBar"><input type="search" placeholder="검색"></input></div>
+        <div className="nav_item">
+          <i className="far fa-compass"></i>
+          <i className="far fa-heart"></i>
+          <i className="far fa-user"></i>
+        </div>
+      </div>
+
+      <div className="super_main">
+        <div className="main_left">
+          <div className="left_top">
+            <div className="story_one">
+              <img src="imgs/yj.png" alt="#" width="85" height="85"></img>
             </div>
-        );
-    }
+            <div className="story_name">
+              <span>yjnim</span>
+            </div>
+          </div>
+          <div className="feeds">
+            <div className="feeds_top">
+              <img src="imgs/yj.png" alt="#" width="65" height="65"></img>
+                <div className="feeds_top_left">
+                  <div className="img_id">kingyjnim</div>
+                  <div className="img_ex">행복</div>
+                </div>
+                <div className="img_imo"><i class="far fa-star"></i></div>
+            </div>
+            <div className="feeds_img"><img src="imgs/ciga.png" alt="ciga" width="780" height="700"></img></div>
+            <div className="feeds_imo">
+                <i className="far fa-heart"></i>
+                <i className="far fa-comment"></i>
+                <i className="far fa-paper-plane"></i>
+              <div className="feeds_imo_right">
+                <i className="far fa-bookmark"></i>
+              </div>
+            </div>
+            <div className="feeds_write"></div>
+              <div className="comment">
+                <input type="text" className="add_comment" placeholder="댓글달기.."></input>
+                <button className="add_button">게시</button>
+              </div>
+          </div>
+        </div>
+
+        <div className="main_right">
+          <div className="main_right_top">
+            <div className="main_right_top_img">
+              <img src="imgs/yj.png" alt="#" width="85" height="85"></img>
+            </div>
+              <div className="main_right_top_name">
+                <div>kimgyjnim</div>
+                <div className="right_top_myName">김영재</div>
+              </div>
+              <div className="main_right_top_tag">전환</div>
+          </div>
+          <div className="main_right_bottom">
+            <div className="main_right_bottom_recommend">회원님을 위한 추천</div>
+            <div className="main_right_bottom_show">모두 보기</div>
+          </div>
+          <div className="footer">
+            <div>소개.도움말.홍보 센터.API.채용 정보.개인정보처리방침</div>
+            <div>약관.위치.인기계정.해시태그.언어</div>
+            <div>© 2020 INSTAGRAM FROM FACEBOOK</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+  }
 }
 
-export default Main;        
+export default Main;
